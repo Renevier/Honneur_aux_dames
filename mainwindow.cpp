2598e1c88104e00e3eb840538e5e0fc8c9b33dec
jeremy_renevier <renevier.jeremy@gmail.com>
2021-01-25 23:59:00 +0100

    fit the bc

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,5 +1,6 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QGraphicsPixmapItem>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -7,10 +8,16 @@ MainWindow::MainWindow(QWidget *parent)
 {
    ui->setupUi(this);
 
-   scene = new QGraphicsScene(this);
+   this->scene = new QGraphicsScene(this);
+   this->scene->setSceneRect(-600, -300, 1200, 600);
 
+   QGraphicsPixmapItem* pixItem = new QGraphicsPixmapItem(QPixmap(":/Ressources/Board.png"));
+   this->scene->addItem(pixItem);
+   pixItem->setPos(QPointF(0, 0) - QPointF(pixItem->boundingRect().width() / 2,
+                                           pixItem->boundingRect().height() / 2));
 
-   ui->graphicsView->setScene(scene);   
+   ui->graphicsView->setScene(scene);
+
 }
 
 MainWindow::~MainWindow()
